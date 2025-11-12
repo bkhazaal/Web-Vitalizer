30b0695249aad013e78b9989b7283c8c40cbda60
Benjamin <benjaminsawesometemporaryaccount@Benjamins-MacBook-Air.local>
2025-11-12 12:47:00 -0700

    server-test

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// load environment variables from .env file
 dotenv.config();
 
+// __dirname replacement in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
